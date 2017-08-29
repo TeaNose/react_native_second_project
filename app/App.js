9c989f842c6c33d7e8611350c0d4b5bad39bde91
Agustinus Wikrama <agustinuswikrama@gmail.com>
2017-08-29 22:23:47 +0700

    Added first page

---
+++ app/App.js
@@ -0,0 +1,16 @@
+import React, {Component} from 'react';
+import {
+	Text,
+	View,
+	Button
+} from 'react-native';
+
+export default class App extends Component{
+	render(){
+		return (
+			<View>
+				<Button onPress="" title="Start" accessibilityLabel="Click this to go to next page"/>
+			</View>
+		)
+	}
+}
